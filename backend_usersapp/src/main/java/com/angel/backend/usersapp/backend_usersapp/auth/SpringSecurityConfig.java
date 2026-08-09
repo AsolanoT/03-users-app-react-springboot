c1e3563984cfcd9147bc817adf998171c262843e
AsolanoT <agsolano-2023a@corhuila.edu.co>
2026-08-09 13:15:07 -0500

    feat: add JWT authentication filter and configure Spring Security

---
+++ backend_usersapp/src/main/java/com/angel/backend/usersapp/backend_usersapp/auth/SpringSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.angel.backend.usersapp.backend_usersapp.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.angel.backend.usersapp.backend_usersapp.auth.filters.JwtAuthenticationFilter;
+
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    SpringSecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
+                .anyRequest().authenticated()
+            )
+                .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
+                .csrf(config -> config.disable())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}
